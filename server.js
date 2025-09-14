7e26070eedcb76d4e5146058d8133ae592b41c0a
FacundoCedolini <fcedolini@iiconcepcion.edu.ar>
2025-09-14 20:07:17 -0300

    Servidor andando.

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
+// server.js
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
-const servidor = http.createServer((req, res) =>{
 
+const servidor = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+
+  // Ruta principal
+  if (req.method === 'GET' && parsedUrl.pathname === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>Servidor funcionando</h1>');
+    res.end();
+  }
 });
-servidor.listen(3000, () => {
-console.log("Servidor ejecutándose");
+
+const PORT = 3000;
+servidor.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
